95cb53924cf12a0003989ce464ca6aa9bf18913e
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-02-12 23:03:54 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MeteorShower from "./Components/MeteorShower";
 import SpaceShip from "./Components/SpaceShip";
-import { isVisible } from "@testing-library/user-event/dist/utils";
 
 function App() {
     return (
