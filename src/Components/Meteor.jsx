120c13f4d31de2e23478894b2e7e4df4fb863459
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-02-12 23:27:58 +0100

    GG

--- src/Components/Meteor.jsx
+++ src/Components/Meteor.jsx
@@ -1,53 +1,53 @@
 import React, { useState } from "react";
 
 function Meteor({ position }) {
-    const GameOver = () => {
-        console.log("ciao");
-    };
-
     return (
-        <div
-            /* onMouseEnter={() => {
+        <>
+            {" "}
+            <div
+                /* onMouseEnter={() => {
                 console.log("ciao");
             }} */
-            onMouseEnter={() => {
-                GameOver();
-            }}
-            className="Meteor-animation position-relative vieniInSuperfice"
-            style={{ position: "absolute", top: `${position}%` }}
-        >
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="fire"></div>
-            <div className="meteor position-absolute top-custom  start-custom"></div>
-        </div>
+                onMouseEnter={() => {
+                    /* GameOver(); */
+                    console.log("KABOOM!");
+                }}
+                className="Meteor-animation position-relative vieniInSuperfice"
+                style={{ position: "absolute", top: `${position}%` }}
+            >
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="fire"></div>
+                <div className="meteor position-absolute top-custom  start-custom"></div>
+            </div>
+        </>
     );
 }
 
